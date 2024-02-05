d4c2f6f9306fdd62b177519588135a978ed36cb7
KazariEX <1364035137@qq.com>
2024-02-06 00:41:57 +0800

    init: 初始化配置

---
+++ index.js
@@ -0,0 +1,11 @@
+export recommended from "./lib/recommended.js";
+import standard from "./lib/standard.js";
+import stylistic from "./lib/stylistic.js";
+import vue from "./lib/vue.js";
+
+export default {
+    recommended,
+    standard,
+    stylistic,
+    vue
+};
