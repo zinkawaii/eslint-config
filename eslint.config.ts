2f973b15a5baa1c295678393b7064af703731f1c
KazariEX <zinkawaii@qq.com>
2025-08-09 22:55:39 +0800

    chore: 更新依赖

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,7 +2,7 @@ import antfu from "@antfu/eslint-config";
 import zin from "./src";
 
 export default antfu({
-    markdown: false,
+    pnpm: true,
     rules: {
         ...zin.standard,
         ...zin.recommended,
