8fe00b977f3c9ae303d0afa5919b6a380428e380
KazariEX <zinkawaii@qq.com>
2026-07-29 17:02:52 +0800

    refactor: 修补 `@antfu/eslint-config` 以移除 `eslint-plugin-unicorn`

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,4 +2,35 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   exports: true,
+  deps: {
+    onlyBundle: [
+      "@antfu/eslint-config",
+    ],
+    neverBundle: [
+      "eslint-plugin-erasable-syntax-only",
+    ],
+  },
+  plugins: [
+    {
+      name: "inject-dependencies",
+      async tsdownConfigResolved(config) {
+        const antfu = await import("@antfu/eslint-config/package.json", {
+          with: { type: "json" },
+        });
+
+        for (
+          const key of [
+            "dependencies",
+            "peerDependencies",
+            "peerDependenciesMeta",
+            "inlinedDependencies",
+          ] as const
+        ) {
+          config.pkg![key] = antfu.default[key];
+        }
+
+        delete config.pkg!.dependencies!["eslint-plugin-unicorn"];
+      },
+    },
+  ],
 });
